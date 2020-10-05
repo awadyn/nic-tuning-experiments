bfac6714f700c1140f5778b00f74c7e3ecc71a19
sanjay <treeinrandomforest@gmail.com>
2020-10-05 17:02:02 -0400

    dataframe from log files

--- simulatedannealing/optim.py
+++ simulatedannealing/optim.py
@@ -7,15 +7,15 @@ import torch.nn as nn
 import torch.optim as optim
 
 from multiprocessing import Manager, Process
+from scipy.stats import entropy
 import glob
 
 plt.ion()
 
+JOULE_CONVERSION = 0.00001526
 TIME_CONVERSION = 1./(2899999*1000)
 KEYS = ['MSG', 'ITR', 'RAPL', 'DVFS']
 METRIC = 'EDP'
-
-COLS = ['i', 'rx_desc', 'rx_bytes', 'tx_desc', 'tx_bytes', 'instructions', 'cycles', 'llc_miss', 'joules', 'timestamp']
 
 '''
 Varying workloads
@@ -210,12 +210,49 @@ class CEM:
         self.data = data
 
 def compute_features_from_logs(loc, N_parallel=4):
+    def compute_entropy(df, colname):
+        x  = df[colname].value_counts()
+
+        x = x / x.sum() #scipy.stats.entropy actually automatically normalizes
+
+        return entropy(x)
+
     def featurize(file, data):
         df = pd.read_csv(file, sep=' ')
+        df_non0j = df[df['joules'] > 0].copy()
+
+        #non-zero joules
+        df_non0j['timestamp'] = df_non0j['timestamp'] - df_non0j['timestamp'].min()
+        df_non0j['joules'] = df_non0j['joules'] - df_non0j['joules'].min()
+        
+        df_non0j['timestamp'] = df_non0j['timestamp'] * TIME_CONVERSION
+        df_non0j['joules'] = df_non0j['joules'] * JOULE_CONVERSION
 
         #add features here
-        d = {'name': file}
-        d['shape'] = df.shape[0]
+        d = {'name': file}        
+        for col in ['rx_desc', 'rx_bytes', 'tx_desc', 'tx_bytes']:
+            d[f'entropy_{col}'] = compute_entropy(df, col)
+
+        tags = file.split('.')[-2]
+        rnd, cpu, msg, nrounds, itr, dvfs, rapl = tags.split('_')
+        d['rnd'] = int(rnd)
+        d['cpu'] = int(cpu)
+        d['msg'] = int(msg)
+        d['nrounds'] = int(nrounds)
+        d['itr'] = int(itr)
+        d['dvfs'] = dvfs
+        d['rapl'] = int(rapl)
+
+        last_row = df_non0j.tail(1).iloc[0]
+        d['joules'] = last_row['joules']
+        d['time'] = last_row['timestamp']
+        d['edp'] = 0.5 * d['joules'] * d['time']
+
+        cols = ['rx_desc', 'tx_desc', 'rx_bytes', 'tx_bytes']
+        corr = df[cols].corr()
+        for i in range(len(cols)):
+            for j in range(i):
+                d[f'corr_{cols[i]}_{cols[j]}'] = corr[cols[i]][cols[j]]
 
         data.append(d)
 
@@ -235,6 +272,11 @@ def compute_features_from_logs(loc, N_parallel=4):
         if N_current % N_parallel == 0:
             [p.join() for p in plist]
 
+
+    data = list(data)
+    data = pd.DataFrame(data)
+    data.set_index(['name', 'rnd', 'cpu', 'msg', 'nrounds', 'itr', 'dvfs', 'rapl'], inplace=True)
+    
     return data
 
 class PolicyGradient:
