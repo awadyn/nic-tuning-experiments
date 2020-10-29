b9a3d7b22d84c8a1b82452971f5bfa3f8c8766c6
sanjay <treeinrandomforest@gmail.com>
2020-10-29 17:25:44 -0400

    adding 3d plots

--- dashboard/dashboard_flex.py
+++ dashboard/dashboard_flex.py
@@ -58,7 +58,20 @@ app.layout = html.Div([
             style={'display': 'inline-block'},
         ),
 
+    ]),
+
+    html.Div([
+        dcc.Dropdown(id='xaxis3d-selector', value='time_mean'),
+        dcc.Dropdown(id='yaxis3d-selector', value='joules_mean'),
+        dcc.Dropdown(id='zaxis3d-selector', value='itr'),
+
+        dcc.Graph(
+            id='custom-scatter3d',
+            style={'display': 'inline-block'},
+        ),
+
     ])
+
 
 ])
 
@@ -77,6 +90,11 @@ def update_radio_button(workload):
     Output('edp-scatter', 'figure'),
     Output('xaxis-selector', 'options'),
     Output('yaxis-selector', 'options'),
+
+    Output('xaxis3d-selector', 'options'),
+    Output('yaxis3d-selector', 'options'),
+    Output('zaxis3d-selector', 'options'),
+
     [Input('workload-selector', 'value'), 
      Input('msg-qps-selector', 'value'), 
      Input('aggregate-error-bar-selector', 'value')]
@@ -127,7 +145,7 @@ def update_edp_scatter(workload, msg, agg_err_bar):
                          title=f'{workload.capitalize()} {msg} bytes Global Plot')
 
 
-    return fig, xaxis_values, yaxis_values #, df_comb.to_dict('records'), [{'name': i, 'id': i} for i in df_comb.columns]
+    return fig, xaxis_values, yaxis_values, yaxis_values, yaxis_values, yaxis_values #, df_comb.to_dict('records'), [{'name': i, 'id': i} for i in df_comb.columns]
 
 @app.callback(
     Output('custom-scatter', 'figure'),
@@ -177,6 +195,65 @@ def update_custom_plot(workload, msg, agg_err_bar, xcol, ycol):
                          title=f'{workload.capitalize()} {msg} bytes Global Plo')
 
     return fig
+
+@app.callback(
+    Output('custom-scatter3d', 'figure'),
+    [Input('workload-selector', 'value'), 
+     Input('msg-qps-selector', 'value'), 
+     Input('aggregate-error-bar-selector', 'value'),     
+     Input('xaxis3d-selector', 'value'),
+     Input('yaxis3d-selector', 'value'),
+     Input('zaxis3d-selector', 'value')]
+)
+def update_custom_plot3d(workload, msg, agg_err_bar, xcol, ycol, zcol):
+    if workload in agg_data:
+        df_comb, df, outlier_list = agg_data[workload]
+    else:
+        df_comb, df, outlier_list = start_analysis(workload=workload)
+        df_comb.reset_index(inplace=True)
+        
+        #TODO: lower dvfs for non-netpipe workloads
+        df_comb['Sys'] = df_comb.apply(lambda x: x['sys'] + ' default' if x['sys']=='linux' and x['dvfs'].lower()=='0xffff' and x['itr']==1 else x['sys'], axis=1)
+
+        agg_data[workload] = (df_comb, df, outlier_list)
+
+    if workload=='netpipe':
+       df_comb = df_comb[df_comb['msg']==msg]
+
+    elif workload=='mcd' or workload=='mcdsilo':
+        df_comb = df_comb[df_comb['QPS']==msg]
+
+    if agg_err_bar=='on':
+        fig = px.scatter_3d(df_comb, 
+                            x=xcol, 
+                            y=ycol, 
+                            z=zcol,
+                            error_x=xcol.replace('_mean', '_std'), 
+                            error_y=ycol.replace('_mean', '_std'), 
+                            error_z=zcol.replace('_mean', '_std'),
+                            color='Sys',
+                            size='edp_mean',
+                            hover_data=hover_data[workload],
+                            custom_data=['itr', 'rapl', 'dvfs', 'Sys', 'sys'],
+                            title=f'{workload.capitalize()} {msg} bytes Global Plo',
+                            width=800,
+                            height=800)
+
+    else:
+        fig = px.scatter_3d(df_comb, 
+                            x=xcol, 
+                            y=ycol, 
+                            z=zcol,
+                            color='Sys',
+                            size='edp_mean',
+                            hover_data=hover_data[workload],
+                            custom_data=['itr', 'rapl', 'dvfs', 'Sys', 'sys'],
+                            title=f'{workload.capitalize()} {msg} bytes Global Plo',
+                            width=800,
+                            height=800)
+
+    return fig
+
 
 if __name__=='__main__':
     app.run_server(debug=True)
