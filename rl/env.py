882dfb82e5ad8e68c5621c357731a35d692e6b26
treeinrandomforest <sra2114@gmail.com>
2021-10-25 17:54:27 -0400

    preliminary env

--- rl/env.py
+++ rl/env.py
@@ -9,13 +9,39 @@ class Workload(gym.Env):
 
         self.state_dict = state_dict 
 
-        self.state = self.init_state()
+        self.state, self.key = self.init_state()
+
+        self.time_limit_sec = 30
+        self.current_time_sec = 0
 
     def step(self, action):
-        
+        #action will be itr delta, rapl delta, dvfs delta
+        itr_delta, dvfs_delta, rapl_delta = action
+
+        new_key = (self.key[0] + itr_delta, self.key[1] + dvfs_delta, self.key[2] + rapl_delta)
+
+        if new_key not in self.state:
+            raise NotImplementedError()
+
+        #compute reward from previous state
+        reward = self.state['joules_per_interrupt']
+
+        #update state
+        self.key = new_key
+        self.state = self.state_dict[self.key]
+
+        self.current_time_sec += self.state['time_per_interrupt']
+
+        done = False
+        if self.current_time_sec >= self.time_limit_sec:
+            done = True
+
+        info = {}
+
+        return self.state, reward, done, info
 
     def reset(self):
-        self.state = self.init_state
+        self.state, self.key = self.init_state()
 
     def render(self):
         pass
@@ -23,7 +49,11 @@ class Workload(gym.Env):
     def init_state(self):
         idx = np.random.randint(len(self.state_dict))
 
-        self.state = self.state_dict[list(self.state_dict.keys())[idx]]
+        key = list(self.state_dict).keys()[idx]
+
+        state = self.state_dict[key]
+
+        return state, key
 
 '''
 workload fixed:
